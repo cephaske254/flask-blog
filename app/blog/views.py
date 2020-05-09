81a2cf73fd2fe6ea04ee9bf1776fe873ac42179d
cephaske254 <cephaske254@gmail.com>
2020-05-09 12:00:16 +0300

    created form UI for adding custom tags

--- app/blog/views.py
+++ app/blog/views.py
@@ -1,7 +1,17 @@
-from flask import render_template,redirect
+from flask import render_template,redirect,request
 from . import blog
 
 @blog.route('/')
 def index():
     title = 'Blog | TRNB'
     return render_template('blog/index.html',nav=True, title=title)
+
+@blog.route('/new-post')
+def new_post():
+    title = 'New Post | TRNB'
+    return render_template('blog/new_post.html',nav=True, title=title)
+
+@blog.route('/new_tag',methods=['GET','POST'])
+def new_tag():
+    if request.environ['HTTP_REFERER'] is not None:
+        return redirect(request.environ['HTTP_REFERER'])
