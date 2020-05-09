dfaf05adce5de28e656c1947e74c74c562e31462
cephaske254 <cephaske254@gmail.com>
2020-05-09 17:28:12 +0300

    added models.py

--- manage.py
+++ manage.py
@@ -1,10 +1,13 @@
 from flask_script import Manager,Server
 from app import create_app
+from flask_migrate import Migrate,MigrateCommand
 
 app = create_app('development')
 manager = Manager(app)
+Migrate(app)
 
 manager.add_command('server',Server)
+manager.add_command('db',MigrateCommand)
 
 if __name__ == '__main__':
     manager.run()
